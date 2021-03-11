f7d02cf9d2022a928e87c1d8d4ac89a867cdf254
Md Aref Billah <mdarefbillah818@gmail.com>
2021-03-11 18:12:03 +0600

    spaced-nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 // import React from 'react';
 import styles from '../styles/Layout.module.css'
 import Nav from './Nav';
+import Header from './Header';
+
 const Layout = ({children}) => {
     return (
        <>
             <Nav></Nav>
+            
             <div className={styles.container} > 
                     {/* <h1>Layout</h1> */}
-                    <main className= {styles.main}>{children}</main>
+                    <main className= {styles.main}>
+                    <Header></Header>{children}</main>
                     
             </div>
         </>
